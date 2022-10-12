a53db341ec7588f21b1ea1b81ef4fe9fbaaff773
orhanbenli <orhanbenli7@outlook.com>
2022-10-12 11:16:43 -0400

    Add some files

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Tilt from "react-parallax-tilt";
+import face from "./face.png";
 import "./Logo.css";
 
 const Logo = () => {
   return (
     <div className="ma4 mt0">
-      <Tilt className="Tilt br2 shadow-2">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 55 }}
+        style={{ height: 150, width: 150 }}
+      >
         <div>
-          <h1>👀</h1>
+          <h1>
+            <img
+              style={{ paddingTop: "25px" }}
+              src={face}
+              alt="face"
+              height={100}
+              width={100}
+            />
+          </h1>
         </div>
       </Tilt>
     </div>
